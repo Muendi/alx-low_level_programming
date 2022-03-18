80e0063c248511474f3258aa5387f2965f2cb7d4
Muendi <muendikyalo@gmail.com>
2022-03-18 11:31:25 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,6 +5,17 @@
  * Return: 0
  */
 
+void positive_or_negative(int n)/**n is a psitive or negative number*/
+{
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+}
+
 int main(void)
 {
 	int i;
@@ -12,6 +23,5 @@ int main(void)
 	i = 0;
 
 	positive_or_negative(i);
-
 	return (0);
 }
